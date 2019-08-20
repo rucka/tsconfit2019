7b470b6cc75de4d08df026d69c82392c2772d6e7
rucka <rucka@tiscalinet.it>
2019-08-20 08:03:26 +0200

    processing orders and get some result....

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('tsconf 2019....')
+import { orders } from './data'
+import { EvaluateOrder } from './api'
+import { processOrderVanilla } from './processOrderVanilla'
+
+const process = (f: EvaluateOrder) => (orderId: string) =>
+  f(orderId)
+    .then(total => console.log(`order ${orderId} total amount: ${total}`))
+    .catch((e: Error) => console.log(`Error order ${orderId} ${e.message}`))
+
+const processOrder = process(processOrderVanilla)
+Promise.all(Object.keys(orders).map(processOrder)).then(() => {
+  console.log('completed...')
+})
