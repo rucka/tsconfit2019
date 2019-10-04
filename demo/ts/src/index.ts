2eed24debf224e770e2736e5007ca62477dd758f
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2019-10-04 16:32:13 +0200

    Added heap usage stats.

--- demo/ts/src/index.ts
+++ demo/ts/src/index.ts
@@ -1,10 +1,17 @@
 import { get as getConfiguration, benchmark } from './configuration'
+
+var memwatch = require('node-memwatch')
 ;(async () => {
+  const heapDiffStart = new memwatch.HeapDiff()
   const configuration = await getConfiguration()
   const [timeInMs, result] = await benchmark(configuration)
-  let iterInUsec = (1000 * timeInMs) / configuration.epoch
+  const heapDiff = heapDiffStart.end()
+  const iterInUsec = (1000 * timeInMs) / configuration.epoch
+  const heapUsage = (heapDiff.after.size_bytes - heapDiff.before.size_bytes)
+    .toString()
+    .padStart(8, ' ')
   console.log(
-    `${configuration.name}\ttime ms ${timeInMs}\t iter us ${iterInUsec}\twarmup \t${configuration.warmup}\titer ${configuration.epoch}\t(ok ${result.ok_counter} ko ${result.ko_counter})\ttotal ${result.total}`
+    `${configuration.name}\ttime ms ${timeInMs}\t iter us ${iterInUsec}\theap ${heapUsage}\twarmup \t${configuration.warmup}\titer ${configuration.epoch}\t(ok ${result.ok_counter} ko ${result.ko_counter})\ttotal ${result.total}`
   )
   process.exit(0)
 })()
